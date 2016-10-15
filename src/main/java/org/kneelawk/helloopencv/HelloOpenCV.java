9ff35be3495198068c4653fe259b6e67bc423cf3
Kneelawk <jedidiah.pommert@gmail.com>
2016-10-15 01:24:28 -0700

    Simple face detection program

--- src/main/java/org/kneelawk/helloopencv/HelloOpenCV.java
+++ src/main/java/org/kneelawk/helloopencv/HelloOpenCV.java
@@ -1,5 +1,7 @@
 package org.kneelawk.helloopencv;
 
+import java.io.File;
+
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
@@ -20,20 +22,49 @@ public class HelloOpenCV {
 
 		CascadeClassifier faceDetector = new CascadeClassifier(HelloOpenCV.class
 				.getResource("/lbpcascade_frontalface.xml").getPath());
-		Mat image = Imgcodecs.imread("images/people-02.png");
+		Mat image = Imgcodecs.imread("images/people.jpg");
+		Mat gray = new Mat();
+		Imgproc.cvtColor(image, gray, Imgproc.COLOR_BGR2GRAY);
 
 		MatOfRect detectedFaces = new MatOfRect();
-		faceDetector.detectMultiScale(image, detectedFaces);
+		faceDetector.detectMultiScale(gray, detectedFaces);
 
-		System.out.println("Detected faces: " + detectedFaces.toArray().length);
+		Rect[] array = detectedFaces.toArray();
+
+		System.out.println("Detected faces: " + array.length);
+
+		int n = 1;
+		String out;
+		while (new File(out = "images/detectedFaces" + n + "/").exists())
+			n++;
+
+		System.out.println("Writing to " + out);
+
+		new File(out).mkdirs();
+
+		for (int i = 0; i < array.length; i++) {
+			Rect rect = array[i];
+			System.out.println("Writing subimage with coords: x:" + rect.x
+					+ " - " + (rect.x + rect.width) + ", y: " + rect.y + " - "
+					+ (rect.y + rect.height));
+			int minx = Math.max(rect.x - rect.width / 2, 0);
+			int maxx = Math.min(rect.x + rect.width + rect.width / 2,
+					image.width());
+			int miny = Math.max(rect.y - rect.height / 2, 0);
+			int maxy = Math.min(rect.y + rect.height + rect.height / 2,
+					image.height());
+			Mat nimg = image.submat(miny, maxy, minx, maxx);
+			Imgcodecs.imwrite(out + "face" + i + ".png", nimg);
+		}
 
 		for (Rect rect : detectedFaces.toArray()) {
 			Imgproc.rectangle(image, new Point(rect.x, rect.y),
 					new Point(rect.x + rect.width, rect.y + rect.height),
-					new Scalar(0, 255, 0));
+					new Scalar(0, 255, 0), 2);
 		}
 
-		Imgcodecs.imwrite("images/detectedFaces3.png", image);
+		Imgcodecs.imwrite(out + "fullImage.png", image);
+		Imgcodecs.imwrite(out + "fullImageGray.png", gray);
 		System.out.println("Detected faces image written");
 	}
 }
