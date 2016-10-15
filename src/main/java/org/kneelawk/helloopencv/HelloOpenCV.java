59525eafe2d494de1f5f0b6c677011264fc20f16
Kneelawk <jedidiah.pommert@gmail.com>
2016-10-14 23:41:34 -0700

    Simple face detection test

--- src/main/java/org/kneelawk/helloopencv/HelloOpenCV.java
+++ src/main/java/org/kneelawk/helloopencv/HelloOpenCV.java
@@ -1,9 +1,14 @@
 package org.kneelawk.helloopencv;
 
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
 public class HelloOpenCV {
 	static {
@@ -12,12 +17,23 @@ public class HelloOpenCV {
 
 	public static void main(String args[]) {
 		System.out.println("Welcome to OpenCV " + Core.VERSION);
-		Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
-		System.out.println("OpenCV Matrix: " + m);
-		Mat r1 = m.row(1);
-		r1.setTo(new Scalar(1));
-		Mat c5 = m.col(5);
-		c5.setTo(new Scalar(5));
-		System.out.println("Matrix data:\n" + m.dump());
+
+		CascadeClassifier faceDetector = new CascadeClassifier(HelloOpenCV.class
+				.getResource("/lbpcascade_frontalface.xml").getPath());
+		Mat image = Imgcodecs.imread("images/people-02.png");
+
+		MatOfRect detectedFaces = new MatOfRect();
+		faceDetector.detectMultiScale(image, detectedFaces);
+
+		System.out.println("Detected faces: " + detectedFaces.toArray().length);
+
+		for (Rect rect : detectedFaces.toArray()) {
+			Imgproc.rectangle(image, new Point(rect.x, rect.y),
+					new Point(rect.x + rect.width, rect.y + rect.height),
+					new Scalar(0, 255, 0));
+		}
+
+		Imgcodecs.imwrite("images/detectedFaces3.png", image);
+		System.out.println("Detected faces image written");
 	}
 }
